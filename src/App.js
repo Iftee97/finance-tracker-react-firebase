b048d07cbfc5729a084b7709fb11edc9e7e2e6ce
Iftee1711402 <tawsifhaque97@gmail.com>
2022-06-30 18:41:09 +0600

    ...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
-import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// pages & components
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
 
 const App = () => {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      </BrowserRouter>
     </div>
   )
 }
