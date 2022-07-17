0d05cc247f00c682333467544099b5000f3e10c0
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-17 22:59:19 +0600

    ...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 import styles from './Navbar.module.css'
 
 const Navbar = () => {
   const { logout } = useLogout()
+  const { user } = useAuthContext()
 
   return (
     <div className={styles.navbar}>
@@ -13,19 +15,29 @@ const Navbar = () => {
           <Link to='/'>finance tracker</Link>
         </li>
 
-        <li>
-          <Link to='/login'>login</Link>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to='/login'>login</Link>
+            </li>
+            <li>
+              <Link to='/signup'>signup</Link>
+            </li>
+          </>
+        )}
 
-        <li>
-          <Link to='/signup'>signup</Link>
-        </li>
-
-        <li>
-          <button className='btn' onClick={logout}>
-            logout
-          </button>
-        </li>
+        {user && (
+          <>
+            <li>
+              hello, {user.displayName}
+            </li>
+            <li>
+              <button className='btn' onClick={logout}>
+                logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   )
