766cc3f09e5f38b4f7ae4e6c8c5ac759043cdc14
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-18 00:57:53 +0600

    ...

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react"
 import { projectFirestore } from "../firebase/config"
 
-export const useCollection = (collection, _query) => {
+export const useCollection = (collection, _query, _orderBy) => {
   const [documents, setDocuments] = useState(null)
   const [error, setError] = useState(null)
 
@@ -9,6 +9,7 @@ export const useCollection = (collection, _query) => {
   // resulting in an infinite loop
   // _query is an array and is different on every function call
   const query = useRef(_query).current
+  const orderBy = useRef(_orderBy).current
 
   useEffect(() => {
     // collection ref
@@ -16,6 +17,9 @@ export const useCollection = (collection, _query) => {
 
     if (query) {
       ref = ref.where(...query)
+    }
+    if (orderBy) {
+      ref = ref.orderBy(...orderBy)
     }
 
     // get real-time documents
@@ -35,7 +39,7 @@ export const useCollection = (collection, _query) => {
     return () => {
       unsubscribe()
     }
-  }, [collection, query])
+  }, [collection, query, orderBy])
 
   return {
     documents,
