66a6fe2b383e5e7de45edef6edcb5b97d079c32c
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-17 18:33:59 +0600

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBL7W6Lm8KdHfpbYnFMJIkL7_gCSXL-u0",
+  authDomain: "finance-tracker-f692b.firebaseapp.com",
+  projectId: "finance-tracker-f692b",
+  storageBucket: "finance-tracker-f692b.appspot.com",
+  messagingSenderId: "477997431258",
+  appId: "1:477997431258:web:e21633c93b95708ce1e8e3"
+}
