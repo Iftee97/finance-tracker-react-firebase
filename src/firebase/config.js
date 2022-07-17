0a149424f687c14866d57ca6852285689ce6836f
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-17 19:47:08 +0600

    setup firebase firestore and auth  config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,7 @@
+import firebase from "firebase"
+import "firebase/firestore"
+import "firebase/auth"
+
 const firebaseConfig = {
   apiKey: "AIzaSyBBL7W6Lm8KdHfpbYnFMJIkL7_gCSXL-u0",
   authDomain: "finance-tracker-f692b.firebaseapp.com",
@@ -5,4 +9,16 @@ const firebaseConfig = {
   storageBucket: "finance-tracker-f692b.appspot.com",
   messagingSenderId: "477997431258",
   appId: "1:477997431258:web:e21633c93b95708ce1e8e3"
+}
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig)
+
+// initilize firebase services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+export {
+  projectFirestore,
+  projectAuth
 }
