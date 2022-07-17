5f757ae166724cefbfece3e4e811b2ba63c07b4f
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-17 18:56:45 +0600

    added login form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Login.module.css'
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log({ email, password })
+
+    setEmail('')
+    setPassword('')
+  }
+
   return (
-    <div>Login</div>
+    <form className={styles['login-form']} onSubmit={handleSubmit}>
+      <h2>login</h2>
+
+      <label>
+        <span>email:</span>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+
+      <label>
+        <span>password:</span>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+
+      <button className='btn'>login</button>
+    </form>
   )
 }
 
