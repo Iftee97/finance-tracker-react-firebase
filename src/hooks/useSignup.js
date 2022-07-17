659911b72caad5d3c3998b0ad2db648f3cea4a22
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-17 21:26:19 +0600

    context and reducer for LOGIN action

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import { projectAuth } from "../firebase/config"
+import { useAuthContext } from "./useAuthContext"
 
 export const useSignup = () => {
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
+  const { dispatch } = useAuthContext()
 
   const signup = async (email, password, displayName) => {
     setError(null)
@@ -15,7 +17,12 @@ export const useSignup = () => {
       if (!response) {
         throw new Error("could not complete sign up")
       }
+
       await response.user.updateProfile({ displayName })
+
+      // dispatch LOGIN action
+      dispatch({ type: "LOGIN", payload: response.user })
+
       setIsPending(false)
       setError(null)
     } catch (error) {
