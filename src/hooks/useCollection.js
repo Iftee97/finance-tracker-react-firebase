332a9ec2994ce83c0f1f5e746fbbf8c4333b4629
Tawsif Haque <68259102+Iftee97@users.noreply.github.com>
2022-09-24 04:47:34 +0600

    Update useCollection.js

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -8,7 +8,7 @@ export const useCollection = (firestoreCollection, _query, _orderBy) => {
 
   // if we don't use a ref, the query will be re-run every time the component re-renders
   // resulting in an infinite loop
-  // _query and _orderBy are arrays and are different on every function call because they're reference types
+  // _query and _orderBy are arrays and are different on every function call because they are reference types
   const queryRef = useRef(_query).current
   const orderByRef = useRef(_orderBy).current
 
